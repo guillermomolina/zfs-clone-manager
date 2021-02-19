2a90c8667181b3843cfd823728fdc1bef9e1fb11
Guillermo Adrián Molina <guillermoadrianmolina@hotmail.com>
2021-02-19 12:20:40 +0100

    Set valid version

--- zcm/version.py
+++ zcm/version.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "3.0.0-dev"
+__version__ = "3.0.dev0"
 AUTHOR = "Guillermo Adrián Molina"
 AUTHOR_EMAIL = "guillermoadrianmolina@hotmail.com"
 NAME = "zfs-clone-manager"
